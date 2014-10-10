d3ac897b76f9fb48017c0570c771ad0e322c7417
David Grenier <davidgrenier@gmail.com>
2014-10-09 22:38:30 -0400

    Starting for real

--- Validation.fs
+++ Validation.fs
@@ -1,7 +1,50 @@
-﻿namespace Validation
+﻿module Validation.Validation
 
-module OResult =
-    [<Struct>]
-    type Result<'a, 'b, 'c> =
-        val private Result: ('a -> 'c) -> ('b -> 'c) -> 'c
-        private new(result) = { Result = result }
+type Result<'a, 'b> =
+    private
+    | S of 'a
+    | F of 'b * 'b list
+
+let (|Success|Failure|) = function
+    | S a -> Success a
+    | F (err, rest) -> Failure (err :: rest)
+
+module private Seq =
+    let partitioned f xs =
+        Array.foldBack (fun v (first, second) ->
+            match f v with
+            | Choice1Of2 x -> x :: first, second
+            | Choice2Of2 x -> first, x :: second
+        ) (Seq.toArray xs) ([], [])
+
+module R =
+    let succeed value = S value
+    let fail error = F (error, [])
+
+    let map f = function
+        | S x -> succeed (f x)
+        | F (error, rest) -> F (error, rest)
+
+    let mapError f = function
+        | S x -> succeed x
+        | F (error, rest) -> F (f error, List.map f rest)
+
+    let bind f = function
+        | S x -> f x
+        | F (error, rest) -> F (error, rest)
+
+    let sequence results =
+        let successes, failures = Seq.partitioned (|Success|Failure|) results
+        match List.concat failures with
+        | [] -> succeed successes
+        | err :: errors -> F (err, errors)
+
+    type Builder() =
+        member __.Bind(result, f) = bind f result
+        member __.Return value = succeed value
+        member __.ReturnFrom result = result
+
+let result = R.Builder()
+
+let test = Result.fail "Shit"
+
--- Validation.fsi
+++
@@ -1,8 +0,0 @@
-﻿namespace Validation
-
-module OResult =
-    [<NoEquality; NoComparison>]
-    type Result<'a, 'b, 'c> =
-        struct
-            val private Result: ('a -> 'c) -> ('b -> 'c) -> 'c
-        end
