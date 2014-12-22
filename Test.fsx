3fa71e5225eb7489d67abb71029260edc5ac31e9
David Grenier <davidgrenier@gmail.com>
2014-12-22 13:41:44 -0500

    Initial version for which a nuget package is available

--- Test.fsx
+++ Test.fsx
@@ -1,3 +1,11 @@
 ﻿#r @"bin\Validation.dll"
 
 open Validation
+
+let q : Result<string, _> =
+    result {
+        let! x =
+            [Result.fail "test"; Result.fail "toto"]
+            |> Result.concat
+        return! Result.fail "test"
+    }
--- Validation.fs
+++ Validation.fs
@@ -3,29 +3,47 @@
 type Result<'a, 'b> =
     private
     | Success of 'a
-    | Failure of 'b list
+    | Failure of 'b
 
 module Validate =
-    let Is predicate error = function
-        | value when predicate value -> Success value
-        | _ -> Failure [error]
+    let Is predicate error value =
+        if predicate value then Success value
+        else Failure error
 
-    let ensure predicate error = function
-        | value when predicate value -> Success ()
-        | _ -> Failure [error]
+    let ensure predicate error value =
+        if predicate value then Success ()
+        else Failure error
 
     let isnt predicate = Is (predicate >> not)
     let prevent predicate = ensure (predicate >> not)
 
+    let notEmpty (error: 'b) list =
+        match list with
+        | [] -> Failure error
+        | x::xs -> Success (x, xs)
+
+    let single (error: 'b) list =
+        match list with
+        | [x] -> Success x
+        | _ -> Failure error
+
+    let singleton (error: 'b) (x, xs) =
+        match xs with
+        | [] -> Success x
+        | _ -> Failure error
+
 module Result =
     let succeed value = Success value
-    let fail error = Failure [error]
+    let fail error = Failure error
 
-    let unwrap s f = function
+    let unwrap (s: 'a -> 'c) (f: 'b -> 'c) result =
+        match result with
         | Success x -> s x
         | Failure x -> f x
 
-    let map s f = unwrap (s >> succeed) (List.map f >> Failure)
+    let recover (f: 'b -> 'a) = unwrap id f
+
+    let map s f = unwrap (s >> succeed) (f >> Failure)
     let bind f = unwrap f Failure
 
     let concat results =
@@ -33,16 +51,25 @@ module Result =
         ||> Array.foldBack (fun result results ->
             match result, results with
             | Success x, Success xs -> Success (x :: xs)
-            | Success _, Failure errors | Failure errors, Success _ -> Failure errors
-            | Failure xs, Failure es -> Failure (xs @ es)
+            | Success _, Failure errors -> Failure errors
+            | Failure error, Success _ -> Failure [error]
+            | Failure error, Failure errors -> Failure (error :: errors)
         )
 
-    let private (=>) predicate f error = bind (f predicate error)
-
-    let Is predicate error = bind (Validate.Is predicate error)
+    let filter predicate error = bind (Validate.Is predicate error)
     let isnt predicate error = bind (Validate.isnt predicate error)
     let ensure predicate error = bind (Validate.ensure predicate error)
     let prevent predicate error = bind (Validate.prevent predicate error)
+    let notEmpty error = bind (Validate.notEmpty error)
+    let single error = bind (Validate.single error)
+    let singleton error = bind (Validate.singleton error)
+
+    let ofOption error value =
+        match value with
+        | None -> fail error
+        | Some x -> succeed x
+
+    let get result = unwrap id failwith
 
 module Success =
     let map f = Result.map f id
@@ -50,25 +77,23 @@ module Success =
 module Failure =
     let map f = Result.map id f
 
-module Aliased =
-    module V = Validate
-    module R = Result
-
 [<AutoOpen>]
 module TopLevel =
     let (|Success|Failure|) = function
         | Success a -> Success a
         | Failure errors -> Failure errors
     
-    let equalTo = (=)
+    let inline equalTo x y = x = y
     let True x : bool = x
     let False = not
     let inline Zero x = x = LanguagePrimitives.GenericZero
     let inline One x = x = LanguagePrimitives.GenericOne
+    let empty = Seq.isEmpty
 
     type ResultBuilder internal () =
         member __.Return value = Result.succeed value
         member __.ReturnFrom (result: Result<_,_>) = result
         member __.Bind (result, f) = Result.bind f result
+        member __.Bind (result, f) = Result.bind (f >> Failure.map (fun x -> [x])) result
 
     let result = ResultBuilder()
